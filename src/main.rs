2d21ae984403e8f69a60afe772a0f98ede7a7749
Barry Corrigan <b.j.corrigan@gmail.com>
2021-05-15 21:13:05 +0100

    We will try tbot first, unfortunately everything in rust now has annoying async bolted on but we can see how we get on

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![allow(unused_imports)]
 extern crate clap;
+extern crate tbot;
 
 use clap::{App, Arg};
+use tbot::prelude::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("Twat ")
         .version("0.1")
         .author("Barry Corrigan <b.j.corrigan@gmail.com>")
@@ -16,6 +19,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .required(true)
             .takes_value(true))
         .get_matches();
+
+    let mut bot = tbot::from_env!("BOT_TOKEN").event_loop();
 
     Ok(())
 }
