33444d3c693f7c5283f723e1b7f82f138d0febd2
Barry Corrigan <b.j.corrigan@gmail.com>
2021-05-20 21:08:57 +0100

    More stuff

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,24 @@
 extern crate clap;
 extern crate tbot;
 extern crate egg_mode;
+extern crate yansi;
 
 mod config;
+mod twitter;
 
 use clap::{App, Arg};
 use clap::value_t;
 use tbot::prelude::*;
 use tbot::Bot;
 use std::io;
+
+use crate::twitter::Auth;
+
+//parse cfg/rules
+//log onto twitter
+//create handler for twitter events
+//create handler for telegram events
+//the end!
 
 #[tokio::main]
 async fn main() {
@@ -24,11 +34,6 @@ async fn main() {
             .help("The config and rules file which governs which accounts twat uses and what rules it has for following and alerting")
             .required(true)
             .takes_value(true))
-        .arg(Arg::with_name("twitter-auth")
-            .long("twitter-auth")
-            .required(false)
-            .takes_value(false)
-            .help("Perform authentication with twitter to get token"))
         .get_matches();
 
     let cfg_file = Box::leak(value_t!(matches, "config", String).unwrap_or_else(|_| "./config.ron".to_string()).into_boxed_str() );
@@ -38,25 +43,12 @@ async fn main() {
         std::process::exit(1);
     });
 
-    //TODO reimplement to follow this process: https://github.com/egg-mode-rs/egg-mode/blob/master/examples/common/mod.rs
-    if matches.is_present("twitter-auth") {
-        async {
-            let con_token = egg_mode::KeyPair::new(config.twitter.key, config.twitter.secret);
-            let request_token = egg_mode::auth::request_token(&con_token, "oob").await.unwrap();
-            let auth_url = egg_mode::auth::authorize_url(&request_token);
+    //twitter log in
+    let twauth = Auth::load(&config).await;
 
-            println!("Please visit this auth url: {}", auth_url);
-            println!("input pin: ");
-            let mut user_input = String::new();
-            io::stdin().read_line(&mut user_input).unwrap();
-            let pin = user_input.trim();
 
-            let (token, user_id, screen_name) = egg_mode::auth::access_token(con_token, &request_token, verifier).await.unwrap();
-
-            println!("Token {} obtained for user {}", token, user_id);
-
-        }.await;
-    }
+    //set up following
+    //https://github.com/egg-mode-rs/egg-mode/blob/master/examples/stream_filter.rs
 
 
 
